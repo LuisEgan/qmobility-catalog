a3910096656758fd123eb8bf17a44de98b1df123
L <l@cleveritgroup.com>
2020-09-21 19:31:31 -0300

    ♻️ clean

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,6 +1,4 @@
 import React, { FC } from "react";
-// import { useQuery } from "@apollo/react-hooks";
-// import { gql } from "apollo-boost";
 
 import PrivateRoute from "../src/components/Auth/PrivateRoute/PrivateRoute";
 import { IPrivateComponentsDefaults } from "../src/lib/Types";
@@ -9,15 +7,6 @@ interface IHome extends IPrivateComponentsDefaults {}
 
 const Home: FC<IHome> = (props: IHome) => {
   const { token } = props;
-
-  // const { data } = useQuery(gql`
-  //   query Consumers {
-  //     consumers {
-  //       id
-  //       name
-  //     }
-  //   }
-  // `);
 
   return (
     <PrivateRoute>
